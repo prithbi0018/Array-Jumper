1aee1a52b29f08e8f2f462c1151076c59b5fc395
prithbi0018 <prithbirajpanda@gmail.com>
2024-11-01 09:57:24 +0530

    added code for render and update Player

---
+++ Array-Jumper/header/Player/PlayerModel.h
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace Player
+{
+
+	enum class PlayerState
+	{
+		ALIVE,
+		DEAD
+	};
+
+
+	class PlayerModel
+	{
+	private:
+
+		PlayerState player_state;
+		int current_position;
+
+	public:
+		PlayerState getPlayerState();
+		void setPlayerState(PlayerState new_player_state);
+
+		int getCurrentPosition();
+		void setCurrentPosition(int new_position);
+	};
+
+}
---
+++ Array-Jumper/source/Player/PlayerModel.cpp
@@ -0,0 +1,24 @@
+#include "../../header/Player/PlayerModel.h"
+
+namespace Player
+{
+	PlayerState PlayerModel::getPlayerState()
+	{
+		return player_state;
+	}
+
+	void PlayerModel::setPlayerState(PlayerState new_player_state)
+	{
+		player_state = new_player_state;
+	}
+
+	int PlayerModel::getCurrentPosition()
+	{
+		return current_position;
+	}
+
+	void PlayerModel::setCurrentPosition(int new_position)
+	{
+		current_position = new_position;
+	}
+}
