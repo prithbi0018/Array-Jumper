ccd4d0f81b0c0c8f1bd5a3bb7675df5208752ebb
prithbi0018 <prithbirajpanda@gmail.com>
2024-11-05 11:58:05 +0530

    Added Code to draw a Box with Overlay

--- Array-Jumper/header/Level/LevelView.h
+++ Array-Jumper/header/Level/LevelView.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "../../header/UI/UIElement/ImageView.h"
+#include "LevelController.h"
+#include "LevelModel.h"
 
 namespace Level
 {
@@ -10,8 +12,16 @@ namespace Level
 	private:
 		sf::RenderWindow* game_window;
 		LevelController* level_controller;
+		BoxDimensions box_dimenson;
 
 		UI::UIElement::ImageView* background_image;
+		UI::UIElement::ImageView* box_image;
+		UI::UIElement::ImageView* target_overlay_image;
+		UI::UIElement::ImageView* letter_one_overlay_image;
+		UI::UIElement::ImageView* letter_two_overlay_image;
+		UI::UIElement::ImageView* letter_three_overlay_image;
+		UI::UIElement::ImageView* obstacle_one_overlay_image;
+		UI::UIElement::ImageView* obstacle_two_overlay_image;
 		const float background_alpha = 110.f;
 
 		void createImages();
@@ -19,6 +29,10 @@ namespace Level
 		void updateImages();
 		void drawLevel();
 		void deleteImages();
+		void calculateBoxDimensions();
+		UI::UIElement::ImageView* getBoxOverlayImage(BlockType block_type);
+		void drawBox(sf::Vector2f position);
+		void drawBoxValue(sf::Vector2f position, BlockType blockTypeToDraw);
 
 	public:
 		LevelView(LevelController* controller);
